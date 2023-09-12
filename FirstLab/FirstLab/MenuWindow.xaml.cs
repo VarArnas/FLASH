f4794947f0cdae8bbfe76c83428c66ad0e817a3c
VarArnas <144057061+VarArnas@users.noreply.github.com>
2023-09-12 23:28:29 +0300

    check if ignore works

--- FirstLab/FirstLab/MenuWindow.xaml.cs
+++ FirstLab/FirstLab/MenuWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace FirstLab
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuWindow : Window
     {
-        public MainWindow()
+        public MenuWindow()
         {
             InitializeComponent();
         }
