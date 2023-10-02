226bb9e08ffb00f122db4a37e473ad5af8d7e0be
bestoxl <34395084+bestoxl@users.noreply.github.com>
2023-10-02 17:43:01 +0300

    added class for saving and retrieving data from json

---
+++ FirstLab/FirstLab/src/back-end/DataManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FirstLab.src.back_end
+{
+    public class DataManager
+    {
+        private static string PATH = AppDomain.CurrentDomain.BaseDirectory + "../../../DataFiles/";
+        private static string FILE_NAME = "Set";
+
+        public static void SaveFlashcardSet(FlashcardSet flashcardSet)
+        {
+            string json = JsonSerializer.Serialize(flashcardSet);
+            File.WriteAllText(PATH + FILE_NAME + ".json", json);
+        }
+
+        public static FlashcardSet LoadFlashcard(string fileName)
+        {
+            FlashcardSet? flashcardSet = null;
+            if (File.Exists(PATH + fileName))
+            {
+                string jsonContent = File.ReadAllText(PATH + fileName);
+                flashcardSet = JsonSerializer.Deserialize<FlashcardSet>(jsonContent);
+            }
+
+            return flashcardSet;
+        }
+    }
+}
