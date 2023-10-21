a41604ea64eb4a5cb3b9472ca91cedbf6886dfc4
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2023-10-21 20:37:40 +0300

    modified customization window so there is a preview of flashcard color instantly

--- FirstLab/FirstLab/src/front-end/FlashcardCustomization.xaml.cs
+++ FirstLab/FirstLab/src/front-end/FlashcardCustomization.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
 using FirstLab.src.back_end.errorHandling;
 using FirstLab.src.back_end.utilities;
 
@@ -170,6 +172,22 @@ namespace FirstLab
             if (ColorBox.SelectedItem != null)
             {
                 ListBoxItem selectedColorItem = (ListBoxItem)ColorBox.SelectedItem;
+
+                string flashcardColorT = selectedColorItem.ToString();
+                int indexOfColon = flashcardColorT.IndexOf(":");
+
+                if (indexOfColon != -1)
+                {
+                    flashcardColorT = flashcardColorT.Substring(indexOfColon + 2);
+                }
+
+                if (!string.IsNullOrEmpty(selectedColorItem.ToString()))
+                {
+                    SolidColorBrush colorBrush = (SolidColorBrush)new BrushConverter().ConvertFromString(flashcardColorT);
+
+                    QuestionBorder.Background = colorBrush;
+                    AnswerBorder.Background = colorBrush;
+                }
             }
         }
     }
