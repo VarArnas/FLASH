f968e86be34a5a4a83f8d23b81bccfdb07a3b51a
bestoxl <34395084+bestoxl@users.noreply.github.com>
2023-10-22 00:34:53 +0300

    Added project for testing, created tests for flashcard classes

---
+++ FirstLab/FirstLabTesting/FlashcardSetTest.cs
@@ -0,0 +1,56 @@
+﻿using FirstLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLabTesting
+{
+    public class FlashcardSetTest
+    {
+        FlashcardSet flashcardSet;
+
+        public FlashcardSetTest()
+        {
+            flashcardSet = new FlashcardSet();
+        }
+
+        
+        [Theory]
+        [InlineData("test", "test")]
+        [InlineData("test with spaces", "test with spaces")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void FlashcardSet_FlashcardSetName_ReturnString(string input, string expected)
+        {
+            // Arrange
+            flashcardSet.FlashcardSetName = input;
+
+            // Act
+            var result = flashcardSet.FlashcardSetName;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("Set1", true)]
+        [InlineData("Set2", false)]
+        public void FlashcardSet_Equal_ReturnBoolean(string setName, bool expected)
+        {
+            // Arrange
+            flashcardSet.FlashcardSetName = "Set1";
+            FlashcardSet? other = setName != null ? new FlashcardSet { FlashcardSetName = setName } : null; 
+
+            // Act
+            var result = flashcardSet.Equals(other);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+
+    }
+}
---
+++ FirstLab/FirstLabTesting/FlashcardTest.cs
@@ -0,0 +1,86 @@
+﻿using FirstLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLabTesting
+{
+    public class FlashcardTest
+    {
+        Flashcard flashcard;
+        public FlashcardTest() 
+        {
+            flashcard = new Flashcard();
+        }
+
+        [Theory]
+        [InlineData ("test", "test")]
+        [InlineData ("test with spaces", "test with spaces")]
+        [InlineData ("", "")]
+        [InlineData (null, null)]
+        public void Flashcard_FlashcardQuestion_ReturnString(string input, string expected)
+        {
+            // Arrange
+            
+            // Act
+            flashcard.FlashcardQuestion = input;
+            var result = flashcard.FlashcardQuestion;
+            
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", "test")]
+        [InlineData("test with spaces", "test with spaces")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void Flashcard_FlashcardAnswer_ReturnString(string input, string expected)
+        {
+            // Arrange
+
+            // Act
+            flashcard.FlashcardQuestion = input;
+            var result = flashcard.FlashcardQuestion;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", "test")]
+        [InlineData("test with spaces", "test with spaces")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void Flashcard_FlashcardName_ReturnString(string input, string expected)
+        {
+            // Arrange
+
+            // Act
+            flashcard.FlashcardQuestion = input;
+            var result = flashcard.FlashcardQuestion;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", "test")]
+        [InlineData("test with spaces", "test with spaces")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void Flashcard_FlashcardColor_ReturnString(string input, string expected)
+        {
+            // Arrange
+
+            // Act
+            flashcard.FlashcardQuestion = input;
+            var result = flashcard.FlashcardQuestion;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
