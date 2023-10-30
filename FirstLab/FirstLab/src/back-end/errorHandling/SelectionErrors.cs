910fcf408ad5acf27716a9b362747ce40b9379dd
Cioperis <118674199+Cioperis@users.noreply.github.com>
2023-10-30 05:12:19 +0200

    added SelectionErrors class to throw custom errors and log them to a file

---
+++ FirstLab/FirstLab/src/back-end/errorHandling/SelectionErrors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FirstLab.src.back_end.errorHandling
+{
+    public class SelectionErrors : Exception
+    {
+        public SelectionErrors()
+        {
+        }
+
+        public SelectionErrors(string message) : base(message)
+        {
+        }
+
+        public SelectionErrors(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Exception: " + base.Message;
+            }
+        }
+
+        public static void LogException(Exception ex)
+        {
+            string logFilePath = "error.log";
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"[Error] {DateTime.Now}: {ex.Message}");
+                writer.WriteLine(ex.StackTrace);
+                writer.WriteLine();
+            }
+        }
+    }
+}
