55c9e4ad405778dd63fb7e2f76b0e54cb627801f
Cioperis <118674199+Cioperis@users.noreply.github.com>
2023-11-26 16:41:24 +0200

    added post method

--- FirstLab/FirstLabService/Controllers/OpenAIController.cs
+++ FirstLab/FirstLabService/Controllers/OpenAIController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
 using OpenAI_API.Completions;
-//using FirstLab;
+using System.Threading.Tasks;
 
 namespace ChatGPT_App.Controllers
 {
@@ -29,7 +29,32 @@ namespace ChatGPT_App.Controllers
             }
 
             return Ok(outputResult);
+        }
 
+        [HttpPost]
+        [Route("PostChatGPT")]
+        public async Task<IActionResult> PostChatGPT([FromBody] string prompt)
+        {
+            if (string.IsNullOrEmpty(prompt))
+            {
+                return BadRequest("Prompt cannot be empty");
+            }
+
+            string outputResult = "";
+            var openai = new OpenAIAPI("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");
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = prompt;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+            completionRequest.MaxTokens = 1024;
+
+            var completions = await openai.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Completions)
+            {
+                outputResult += completion.Text;
+            }
+
+            return Ok(outputResult);
         }
     }
 }
