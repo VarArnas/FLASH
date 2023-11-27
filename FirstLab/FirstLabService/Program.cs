5f69b7312db7988e2dfe8c664a737ffa8f5bdb1b
Cioperis <118674199+Cioperis@users.noreply.github.com>
2023-11-27 14:15:22 +0200

    added some middleware

--- FirstLab/FirstLabService/Program.cs
+++ FirstLab/FirstLabService/Program.cs
@@ -16,7 +16,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.UseResponseCompression();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
